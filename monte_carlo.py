6a8388d243d44bd0b5e6f1a8eb0e8ca1609779bf
jiali_sheng <jialisheng2002@gmail.com>
2026-01-29 20:09:13 -0800

    Add CBM S_T

--- monte_carlo.py
+++ monte_carlo.py
@@ -1,8 +1,9 @@
+# flake8: noqa
 import numpy as np
 import matplotlib.pyplot as plt
 import yfinance as yf
-import scipy
 from scipy import stats
+import pandas as pd
 
 # Amazon ticker is AMZN
 amzn_df = yf.Ticker("AMZN")
@@ -10,6 +11,7 @@ amzn_df = amzn_df.history(
     start='2020-01-01',
     end='2025-01-01',
     interval='1d')
+print(amzn_df)
 prices = amzn_df['Close'].dropna()
 # use tz_localize to make the index more clean and tidy
 prices.index = prices.index.tz_localize(None)
@@ -51,26 +53,39 @@ plt.show()
 # especially in extremes.
 
 # MLE to get estimated mean and standard deviation
-mu_hat = log_daily_returns.mean()
-sigma_hat = log_daily_returns.std(ddof=0)
+mu_hat = r.mean()
+sigma_hat = r.std(ddof=0)
 
-# Monte Carlo Simulation for the future price paths
+# Monte Carlo Simulation for the future price paths using
+# Discrete-time lognormal random walk
 T = 1260  # Simulate the price in the future 1260 days
 N = 1000  # For each day, we will simulate 1000 times/paths
 S_0 = prices.iloc[-1]  # The last close price in our dataset
 Z = stats.norm.rvs(size=(N, T), random_state=422)  # standard shock for adding volatility
-returns = mu_hat + sigma_hat * Z 
-cum_r = np.cumsum(returns, axis=1)
-S_T = np.empty((N, T+1))
-S_T[:, 0] = S_0
-S_T[:, 1:] = S_0 * np.exp(cum_r)  # Future 5 years prices, 1000 paths for each day
+returns_dbm = mu_hat + sigma_hat * Z 
+cum_r_dbm = np.cumsum(returns_dbm, axis=1)
+S_T_dbm = np.empty((N, T+1))
+S_T_dbm[:, 0] = S_0
+S_T_dbm[:, 1:] = S_0 * np.exp(cum_r_dbm)  # Future 5 years prices, 1000 paths for each day
+
+# Monte Carlo Simulation using Continuous-time lognormal random walk
+mu_d= mu_hat
+sigma_d = sigma_hat
+dt = 1/252
+mu_ann = mu_d / dt
+sigma_ann = sigma_d / np.sqrt(dt)
+returns_cbm = (mu_ann - 0.5 * sigma_ann**2) * dt + sigma_ann * np.sqrt(dt) * Z
+cum_r_cbm = np.cumsum(returns_cbm, axis=1)
+S_T_cbm = np.empty((N, T+1))
+S_T_cbm[:, 0] = S_0
+S_T_cbm[:, 1:] = S_0 * np.exp(cum_r_cbm)
 
 # Analyze the future prices matrix
-mean_prices = S_T.mean(axis=0)  # Get the means of the matrix in row-way
-median_prices = np.median(S_T, axis=0)
-p05 = np.percentile(S_T, 5, axis=0)
-p95 = np.percentile(S_T, 95, axis=0)
-prob_price_below_S0 = (S_T < S_0).mean(axis=0)  # (S_T<S_0) is a boolean matrix
+mean_prices = S_T_cbm.mean(axis=0)  # Get the means of the matrix in row-way
+median_prices = np.median(S_T_cbm, axis=0)
+p05 = np.percentile(S_T_cbm, 5, axis=0)
+p95 = np.percentile(S_T_cbm, 95, axis=0)
+prob_price_below_S0 = (S_T_cbm < S_0).mean(axis=0)  # (S_T<S_0) is a boolean matrix
 
 # mean_prices vs. median_prices
 days = np.arange(len(mean_prices))
@@ -86,15 +101,56 @@ ax3.legend()
 plt.show()
 
 # End-of-horizon metrics
-end_prices = S_T[:, -1] # Last day simulated prices
+end_prices = S_T_cbm[:, -1]  # Last day simulated prices
 expected_end_price = end_prices.mean()
 median_end_price = np.median(end_prices)
 prob_price_below_S0_end_day = (end_prices < S_0).mean()
 
-print(median_end_price, expected_end_price, p05[-1], p95[-1])
+# Payoff at Horizon
+log_payoff = np.log(end_prices/S_0)
+simple_return = end_prices / S_0 - 1
+
+# Risk Metrics for the Simple Return
+alpha = 0.05
+VaR_simple = np.quantile(simple_return, alpha)
+ES_simple = simple_return[simple_return <= VaR_simple].mean()
+prob_loss_simple = (simple_return < 0).mean()
+
+# Risk Metrics for the Log Payoff
+alpha = 0.05
+VaR_log = np.quantile(log_payoff, alpha)
+ES_log = log_payoff[log_payoff <= VaR_log].mean()
+prob_loss = (log_payoff < 0).mean()
+
+# Visualize Horizon payoff and prices
+fig4, ax4 = plt.subplots()
+ax4.hist(end_prices, bins='fd')
+ax4.set_xlabel("Prices at Horizon")
+ax4.set_ylabel("Count")
+plt.xscale("log")
+plt.axvline(0, linestyle="--", linewidth=1)
 
 
+fig5, ax5 = plt.subplots()
+ax5.hist(log_payoff, bins="fd")
+ax5.set_xlabel("Payoff in the horizon (Log-scale)")
+ax5.set_ylabel("Count")
+plt.axvline(0, linestyle="--", linewidth=1)
+plt.show()
 
+# Create a DataFrame
+metrics = {
+    "S0": S_0,
+    "mean_end_price": expected_end_price,
+    "median_end_price": median_end_price,
+    "p05_end_price": np.percentile(end_prices, 5),
+    "p95_end_price": np.percentile(end_prices, 95),
+    "prob_loss_end": prob_loss,
+    "VaR_95_return": VaR_log,
+    "ES_95_return": ES_log
+}
+results_df = pd.DataFrame(metrics, index=[0])
+print(results_df)
 
 
 
